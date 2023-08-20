df39d7857618320ab50180b423f606e07e33961e
Answed <schradearwed@gmail.com>
2023-08-20 11:21:30 +0200

    Added Basic Scripts, playarea, navmesh and input package

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float heatlh;
+    public int coins; // Could be saved sepratly 
+
+    [SerializeField] private int maxHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
