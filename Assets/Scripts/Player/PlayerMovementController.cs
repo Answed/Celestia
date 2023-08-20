7b6ee80e8649a295e01f3002e9e3f358a0412554
Answed <schradearwed@gmail.com>
2023-08-20 13:42:49 +0200

    Changed movement and added double jump

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -9,7 +9,9 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField] private float jumpForce;
 
     private Vector2 movementDirection;
-
+    private float jump = 0;
+    private int jumpcounter;
+    private bool onGround;
     private Rigidbody rb;
 
     // Start is called before the first frame update
@@ -20,11 +22,38 @@ public class PlayerMovementController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(movementDirection.x, 0, movementDirection.y) * movementSpeed;
+        transform.position += new Vector3(movementDirection.x, 0, movementDirection.y) * movementSpeed * Time.deltaTime;
+
+        if ((onGround || jumpcounter <= 1) && jump == 1)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpcounter++;
+            jump = 0;
+        }
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
     {
         movementDirection = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        jump = ctx.ReadValue<float>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            onGround = true;
+            jumpcounter = 0;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            onGround = false;
     }
 }
