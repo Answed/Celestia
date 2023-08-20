53d1be03b07d618356740313653834e750b97305
Answed <schradearwed@gmail.com>
2023-08-20 12:04:55 +0200

    Added Movement

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,22 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
     [SerializeField] private float jumpForce;
 
+    private Vector2 movementDirection;
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        rb.velocity = new Vector3(movementDirection.x, 0, movementDirection.y) * movementSpeed;
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        movementDirection = ctx.ReadValue<Vector2>();
     }
 }
