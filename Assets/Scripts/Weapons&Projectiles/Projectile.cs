ace3f575564b79f14b49c32b4ae189498e5caa76
Answed <schradearwed@gmail.com>
2023-08-22 11:41:51 +0200

    Projectiles get destroyed after some time

--- Assets/Scripts/Weapons&Projectiles/Projectile.cs
+++ Assets/Scripts/Weapons&Projectiles/Projectile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float throwForce;
+    [SerializeField] private float destroyDelay;
 
     private Rigidbody rb;
 
@@ -16,5 +17,11 @@ public class Projectile : MonoBehaviour
     public void Throw(Vector3 direction)
     {
         rb.AddForce(direction * throwForce, ForceMode.Impulse);
+    }
+
+    IEnumerator DestroyAfterTime()
+    {
+        yield return new WaitForSeconds(destroyDelay);
+        Destroy(gameObject);
     }
 }
