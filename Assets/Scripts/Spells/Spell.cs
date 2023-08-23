0e3bb457f66e4c747decd099ab2f6c681cdf8749
Answed <schradearwed@gmail.com>
2023-08-23 16:51:42 +0200

    Added SpellTemplate

---
+++ Assets/Scripts/Spells/Spell.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell : MonoBehaviour
+{
+    public float spellDamage;
+    public float spellCooldown;
+    public GameObject[] projectiles;
+    public GameObject[] effects;
+}
